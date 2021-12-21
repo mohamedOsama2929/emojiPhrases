dacadd6335fd9e20db638e8aa5e4705fb4d55d45
Mohamed Osama <monkosama1996@gmail.com>
2021-12-21 15:50:04 +0200

    update hello world

--- src/main/kotlin/com/example/api/response/Response.kt
+++ src/main/kotlin/com/example/api/response/Response.kt
@@ -1,8 +1,11 @@
 package com.example.api.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Response<T> (
     val success:Boolean?=null,
     val code:Int?=null,
     val message:String?="",
     val data:T?=null
-        )
+        ):java.io.Serializable
